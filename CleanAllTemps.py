cddf73877b29ece5716e6424ea675721c69b560b
pyro <113712816+pyroalw@users.noreply.github.com>
2023-02-01 18:48:57 +0300

    Add files via upload

---
+++ CleanAllTemps.py
@@ -0,0 +1,77 @@
+import os
+import time
+import colorama
+import pystyle
+
+from colorama import *
+from pystyle import *
+from requests import get, post
+from random import randint
+import requests
+
+HOME1 = """
+                ██████╗ ██╗   ██╗██████╗  ██████╗ ██████╗      
+                ██╔══██╗╚██╗ ██╔╝██╔══██╗██╔═══██╗╚════██╗     
+                ██████╔╝ ╚████╔╝ ██████╔╝██║   ██║  ▄███╔╝     
+                ██╔═══╝   ╚██╔╝  ██╔══██╗██║   ██║  ▀▀══╝      
+                ██║        ██║   ██║  ██║╚██████╔╝  ██╗███████╗
+                ╚═╝        ╚═╝   ╚═╝  ╚═╝ ╚═════╝   ╚═╝╚══════╝
+                                               
+              ⌜――――――――――――――――――――――――――――――――――――――――――――――――――――⌝              
+              ┇                AUTO EASY TEMP CLEANER              ┇
+              ⌞――――――――――――――――――――――――――――――――――――――――――――――――――――⌟
+                              › Press Enter...
+"""
+from tkinter import * 
+from tkinter import messagebox
+print("ALERT: YOU NEED TO JOIN + FOLLOW THESE ACCOUNTS!!!")
+print("You can contact me from these accounts to advertise here!")
+print("Please Wait.")
+
+time.sleep(1)
+print("4")
+time.sleep(1)
+print("3")
+time.sleep(1)
+print("2")
+time.sleep(1)
+print("1")
+time.sleep(1)
+os.system('start https://instagram.com/c4gwn')
+os.system('start https://discord.gg/pyr0')
+time.sleep(10)
+os.system('cls')
+Anime.Fade(Center.Center(HOME1), Colors.rainbow, Colorate.Vertical, interval=0.020, enter=True)
+import tkinter
+messagebox.showwarning("Run as administrator!", "Please run this app as administrator!")
+os.system('DEL /F /Q /A %temp%')
+messagebox.showinfo("Success!", "Cleaned UserTempFolder")
+os.system('DEL /F /Q /A C:\Windows\Temp')
+messagebox.showinfo("Success!", "Cleaned WindowsTemps")
+time.sleep(2)
+os.system('/s /f /q c:\windows\temp\*.*')
+os.system('rd /s /q c:\windows\temp')
+os.system('md c:\windows\temp')
+os.system('del /s /f /q C:\WINDOWS\Prefetch')
+os.system('del /s /f /q %temp%\*.*')
+os.system('rd /s /q %temp%')
+os.system('md %temp%')
+os.system('deltree /y c:\windows\tempor~1')
+os.system('deltree /y c:\windows\temp')
+os.system('deltree /y c:\windows\tmp')
+os.system('deltree /y c:\windows\ff*.tmp')
+os.system('deltree /y c:\windows\history')
+os.system('deltree /y c:\windows\cookies')
+os.system('deltree /y c:\windows\recent')
+os.system('deltree /y c:\windows\spool\printers')
+os.system('del c:\WIN386.SWP')
+os.system('cls')
+os.system(':noAdmin')
+os.system('echo You must run this script as an Administrator !')
+os.system('echo ^<press any key^>')
+os.system('cls')
+messagebox.showinfo("Success!", "Cleaned Caches")
+messagebox.showinfo("Success!", "Cleaned BrowserCaches")
+messagebox.showinfo("Success!", "Cleaned SteamTemps")
+messagebox.showinfo("Success!", "Cleaned DiscordTemps")
+messagebox.showinfo("Join", "discord.gg/pyr0")
